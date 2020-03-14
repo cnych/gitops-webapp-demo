ba2c1d2f3e98a5a37356534d5365bc8fab294feb
Andrzej Kaczyński <andrzej.kaczynski@pragmaticcoders.com>
2020-03-14 17:31:52 +0100

    Changed display name to andrew

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func getPort() string {
 }
 
 func main() {
-   welcome := Welcome{"PROD", time.Now().Format(time.Stamp), os.Getenv("HOSTNAME")}
+   welcome := Welcome{"ANDREW", time.Now().Format(time.Stamp), os.Getenv("HOSTNAME")}
 
    templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
 
