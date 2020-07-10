b0df00380a59e326018e0b1765433618bc4ae29b
yangchuanhu <ych_1024@163.com>
2020-07-10 19:08:42 +0800

    change welcome var to GITOPS-K8S

--- main.go
+++ main.go
@@ -1,46 +1,46 @@
 package main
 
 import (
-   "net/http"
-   "fmt"
-   "os"
-   "time"
-   "html/template"
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+	"time"
 )
 
 type Welcome struct {
-   Name string
-   Time string
-   Pod string
+	Name string
+	Time string
+	Pod  string
 }
 
 func getPort() string {
-  p := os.Getenv("APP_PORT")
-  if p != "" {
-    return ":" + p
-  }
-  return ":8080"
+	p := os.Getenv("APP_PORT")
+	if p != "" {
+		return ":" + p
+	}
+	return ":8080"
 }
 
 func main() {
-   welcome := Welcome{"GITOPS", time.Now().Format(time.Stamp), os.Getenv("HOSTNAME")}
+	welcome := Welcome{"GITOPS-K8S", time.Now().Format(time.Stamp), os.Getenv("HOSTNAME")}
 
-   templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
+	templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
 
-   http.Handle("/static/",
-      http.StripPrefix("/static/",
-         http.FileServer(http.Dir("static"))))
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("static"))))
 
-   http.HandleFunc("/" , func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-      if name := r.FormValue("name"); name != "" {
-         welcome.Name = name;
-      }
-      if err := templates.ExecuteTemplate(w, "welcome-template.html", welcome); err != nil {
-         http.Error(w, err.Error(), http.StatusInternalServerError)
-      }
-   })
+		if name := r.FormValue("name"); name != "" {
+			welcome.Name = name
+		}
+		if err := templates.ExecuteTemplate(w, "welcome-template.html", welcome); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
 
-   fmt.Println("Listening on port " + getPort());
-   fmt.Println(http.ListenAndServe(getPort(), nil));
+	fmt.Println("Listening on port " + getPort())
+	fmt.Println(http.ListenAndServe(getPort(), nil))
 }
